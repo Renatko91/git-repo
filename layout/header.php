e31ef6564174df6472ce3f22d2da9defe40e4e26
Renatko91 <Renatko91@bk.ru>
2018-06-28 17:56:06 +0400

    layouts

---
+++ layout/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+        <link href="style.css" rel="stylesheet">
+        <title>Страница 1</title>
+    </head>
+
+    <body>
+        <div id='header'>
+            Тестовый сайт
+            <a href="index.php">Главная</a> <a href="page1.php">Страница 1</a> <a href="page2.php">Страница 2</a>
+        </div>
